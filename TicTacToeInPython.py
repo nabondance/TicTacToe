a3e404c05b2fe722145e6618c1728a78660fdd17
Nathan Abondance <spykie.me@gmail.com>
2018-04-11 17:07:13 +0200

    .

--- TicTacToeInPython.py
+++ TicTacToeInPython.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 from random import randint
-#Wahou 2 dfddd
+#Wahou 2 dfdddd
 ######################### CONST #########################
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
